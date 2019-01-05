5f4ff2c718cede7702181ac7ae47a3ccbcd283b7
captainglen <34007599+captainglen@users.noreply.github.com>
2019-01-05 07:26:33 -0800

    Update OLED_GEIGER_X.ino

--- OLED_GEIGER_X.ino
+++ OLED_GEIGER_X.ino
@@ -2,19 +2,16 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-
 #define OLED_MOSI  11   //D1
 #define OLED_CLK   13   //D0
 #define OLED_DC    9
 #define OLED_CS    10
 #define OLED_RESET 8
 Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
-
 #define NUMFLAKES 10
 #define XPOS 0
 #define YPOS 1
 #define DELTAY 2
-
 const int geigerPin=2;
 const int buzzerPin=5;   // speaker output
 const int ledPin=6;
@@ -22,7 +19,6 @@ long update=0;
 long counts=0;
 long previouscounts=0;
 long previousUpdate=0;
-long Interval=15000;
 
 void setup() {
   Serial.begin(9600);
@@ -32,11 +28,16 @@ void setup() {
   pinMode(ledPin, OUTPUT);
   digitalWrite(geigerPin, HIGH);
   attachInterrupt(0, event, CHANGE);
+  display.clearDisplay();
+  display.setCursor(10,8);
+  display.println("uSv/h = ");
+  display.setCursor(10,18);	
+  display.println("CPM = ");
 }
 
 void loop() {
   unsigned long currentMilis = millis();
-  if (currentMilis - previousUpdate >= 20000) {
+  if (currentMilis - previousUpdate >= 10000) {
     previousUpdate = currentMilis, update = counts - previouscounts, previouscounts = counts;
     float ET = millis();
 
@@ -50,7 +51,7 @@ void loop() {
     display.setCursor(10,18);	
     display.println("CPM = ");
     display.setCursor(50,18);	
-    display.println(update * 3);
+    display.println(update * 6);
     display.display();
   }
 }
